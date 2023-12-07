7ffa81ea22b75d99d87945892f3aead039006c38
Jacksonnavigator <135830277+Jacksonnavigator@users.noreply.github.com>
2023-12-07 12:11:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ users = {}  # Dictionary to store user accounts
 def main():
     st.title("Collaborative Online Book Store")
 
-    logged_in = False
-    username = st.sidebar.text_input("Username")
-    password = st.sidebar.text_input("Password", type="password")
-    
-    if st.sidebar.button("Login"):
-        logged_in = login(username, password)
-        if not logged_in:
-            st.sidebar.error("Invalid username or password.")
+    login_state = get_login_state()
 
-    if not logged_in:
+    if not login_state["logged_in"]:
+        login_page(login_state)
         st.stop()
 
     operation = st.sidebar.selectbox("Operation", ["View Books", "Order Books", "Manage Books"])
@@ -27,10 +21,58 @@ def main():
     elif operation == "Manage Books":
         manage_books()
 
+def get_login_state():
+    login_state = {"logged_in": False, "username": None}
+
+    if "login_state" in st.session_state:
+        login_state = st.session_state.login_state
+
+    return login_state
+
+def set_login_state(logged_in, username):
+    st.session_state.login_state = {"logged_in": logged_in, "username": username}
+
 def login(username, password):
     if username in users and users[username] == password:
+        set_login_state(True, username)
         return True
     return False
+
+def logout():
+    set_login_state(False, None)
+
+def register(username, password):
+    if username in users:
+        return False
+
+    users[username] = password
+    set_login_state(True, username)
+    return True
+
+def login_page(login_state):
+    st.header("Login")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if login(username, password):
+            st.success("Logged in successfully.")
+        else:
+            st.error("Invalid username or password.")
+
+    st.markdown("---")
+
+    st.header("Register")
+
+    new_username = st.text_input("New Username")
+    new_password = st.text_input("New Password", type="password")
+
+    if st.button("Register"):
+        if register(new_username, new_password):
+            st.success("Registered and logged in successfully.")
+        else:
+            st.error("Username already taken.")
 
 def view_books():
     st.header("Available Books")
@@ -43,7 +85,6 @@ def view_books():
 
 def order_books():
     st.header("Order Books")
-    
 
 def manage_books():
     st.header("Manage Books")
